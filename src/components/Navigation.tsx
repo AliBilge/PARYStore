a853ae1ad08a604af775568678f715e21f5755da
vincentcui5296 <vincentcui5296@gmail.com>
2020-04-21 11:46:18 -0600

    make NavBar responsive

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import {Button} from 'semantic-ui-react';
+import {Button, Responsive, Segment} from 'semantic-ui-react';
 
 const NavButtons = () => (
-    <Button.Group fluid color='violet'>
-      <Button>PARYStore</Button>
-      <Button>Home</Button>
-      <Button>Contact</Button>
-      <Button>Hello</Button>
-      <Button>Logout</Button>
-    </Button.Group>
-  )
+  <div>
+    <Responsive maxWidth={768}>
+      <Button.Group textAlign='center' vertical centered fluid color='violet'>
+        <Button textAlign='center'>PARYStore</Button>
+        <Button>Home</Button>
+        <Button>Contact</Button>
+        <Button>Hello</Button>
+        <Button>Logout</Button>
+      </Button.Group>
+    </Responsive>
+    <Responsive minWidth={768}>
+      <Button.Group fluid color='violet'>
+        <Button>PARYStore</Button>
+        <Button>Home</Button>
+        <Button>Contact</Button>
+        <Button>Hello</Button>
+        <Button>Logout</Button>
+        </Button.Group>
+    </Responsive>
+  </div>
+)
 
   export default NavButtons
