d8e16e4140ee4feb62ab9f46d81a4a4648b651d3
vincentcui5296 <vincentcui5296@gmail.com>
2020-04-21 12:44:07 -0600

    Revert "make NavBar responsive"
    This reverts commit a853ae1ad08a604af775568678f715e21f5755da.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import {Button, Responsive, Segment} from 'semantic-ui-react';
+import {Button} from 'semantic-ui-react';
 
 const NavButtons = () => (
-  <div>
-    <Responsive maxWidth={768}>
-      <Button.Group textAlign='center' vertical centered fluid color='violet'>
-        <Button textAlign='center'>PARYStore</Button>
-        <Button>Home</Button>
-        <Button>Contact</Button>
-        <Button>Hello</Button>
-        <Button>Logout</Button>
-      </Button.Group>
-    </Responsive>
-    <Responsive minWidth={768}>
-      <Button.Group fluid color='violet'>
-        <Button>PARYStore</Button>
-        <Button>Home</Button>
-        <Button>Contact</Button>
-        <Button>Hello</Button>
-        <Button>Logout</Button>
-        </Button.Group>
-    </Responsive>
-  </div>
-)
+    <Button.Group fluid color='violet'>
+      <Button>PARYStore</Button>
+      <Button>Home</Button>
+      <Button>Contact</Button>
+      <Button>Hello</Button>
+      <Button>Logout</Button>
+    </Button.Group>
+  )
 
   export default NavButtons
