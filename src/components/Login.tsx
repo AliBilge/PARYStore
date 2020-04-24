5aa22ab4397cd832c0b1ff363b428ec7e3b10d0e
Rathi <rathiram7583@gmail.com>
2020-04-23 18:06:26 -0600

    Added Login Validation

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import {Button,Header, Container, Input,Label} from 'semantic-ui-react';
+import {Button,Label,Form,Header} from 'semantic-ui-react';
  
+const userPwd: {[index: string]: string} = {
+    Rathi: "paryrathi",
+    Ali:   "paryali",
+    Poupak: "parypoupak",
+    Vincent:"paryvincent"
+}
 
 interface IProps
 {
@@ -9,7 +15,9 @@ interface IProps
 interface IState{
     username:string,
     password:string,
-    submitted:boolean
+    submitted:boolean,
+    usernameErr:boolean,
+    passwordErr:boolean
 }
 
 class Login extends Component<IProps,IState>
@@ -21,81 +29,94 @@ class Login extends Component<IProps,IState>
                              this.state = {
                                       username:'',
                                       password:'',
-                                      submitted:false
+                                      submitted:false,
+                                      usernameErr:false,
+                                      passwordErr:false
 
                                          };
-                            this.handleUsername=this.handleUsername.bind(this);
-                            this.handlePassword=this.handlePassword.bind(this);             
-                            this.handleSubmit=this.handleSubmit.bind(this);
+                                         
+           
+                        
                 }
 
-
-handleUsername =   ( e :any):void =>
-    {
-       this.setState({
-           username:e.target.value
-       });
-        console.log(this.state);
-    }
-    handlePassword=   ( e :any):void =>
-    {
-       this.setState({
-           password:e.target.value
-       });
-        console.log(this.state.username);
-        console.log(this.state.password);
-
-
-    }
-    
-
-
-
+                
 
     handleSubmit = ( e:any ):void =>
     {
         e.preventDefault();
-
-        this.setState (
-                         { submitted :true });
-                      console.log(this.state.submitted);   
+             this.setState({
+            passwordErr: false,
+            usernameErr: false
+                   });
+               
+                 const usernameInput:HTMLInputElement |null =document.querySelector("[name='username']");
+                 const passwordInput:HTMLInputElement |null =document.querySelector("[name ='password']");
+                 if((usernameInput !=null)  && (passwordInput !=null))
+                {
+                                   
+                          for (let user in userPwd)
+                          {
+                               if(user === usernameInput.value)
+                              {
+                                  if(userPwd[user]===passwordInput.value)
+                                  {
+                                      
+                                       this.setState (
+                                        { 
+                                
+                                            username:user,
+                                            submitted :true,
+                                            usernameErr:false,
+                                            passwordErr:false });
+                                        return ;       
+                                   }
+                                
+                                else
+                                {
+                                       this.setState({
+                                        passwordErr:true
+                                        
+                                    });
+                                    
+                               return;
+                                } 
+                                
+                              }
+                              this.setState({
+                                usernameErr:true
+                           
+                         });
                         
+                         }
+                      }
+                           
   }
 
 
-
+  clearInput = () => this.setState({ username: '', password: '' }) 
     render(){
 
-            
-            
+//const { username,password,submitted} =this.state;
+       return(
 
-        const { username,password,submitted} =this.state;
-        return(
-        <div className="col-md-6 col-md-offset-3">
-            <Container fluid>
-               <form name="form" onSubmit={this.handleSubmit} >
-                <Header as='h1'>Login</Header>
-                <div className="input-field">
-                    <Label htmlFor="username" color='violet'>Username:</Label>
-                    <Input type="text" name="username" onChange={this.handleUsername}  value={this.state.username}/>
-                        {submitted && !username &&
-                        <div className="help-block">Username is required
-                        </div>
-                        }
-                </div><br/>
-                <div className="input-field">
-                    <Label htmlFor="password"  color='violet' >Password :</Label>
-                    <Input type="password" name="password" onChange={this.handlePassword} value={this.state.password}/>
-                    {submitted && !password &&
-                     <div className="help-block">Password is required
-                    </div>
-                    }
-                </div><br/>
-                <div className="input-field">
-                <Button basic color='violet' content='Login' />
-               </div>
-            </form>
-            </Container>
+              
+               <div  className="col-md-6 col-md-offset-3">
+                
+                <Form size="large" onSubmit={this.clearInput}>
+                <Header as ="h1">Login Page</Header>
+                <Form.Group  >
+                <Label htmlFor="username" color='violet' size="large" >Username:</Label>
+                <Form.Input   type="text" name="username" placeholder="username"  error={this.state.usernameErr ? "User Name doesn't exist!" : null} width={10} />
+                </Form.Group>
+                <Form.Group >
+                 <Label htmlFor="password"  color='violet' size="large" >Password :</Label>
+                 <Form.Input   type="password" name="password" placeholder="password"  error={this.state.passwordErr ? " Password incorrect" : null} width={10} />
+            
+                 </Form.Group>
+              <Button basic color='violet' content='Login' onClick={this.handleSubmit} />
+              
+              </Form>
+        
         </div>
         )}
 }
