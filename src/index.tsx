5f57612bdd0f44911803f4a2574faeb5584ad1d9
vincentcui5296 <vincentcui5296@gmail.com>
2020-04-27 14:50:53 -0600

    fix some errors

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,6 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import { Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import store from './store';
 
 export const history = createBrowserHistory();
