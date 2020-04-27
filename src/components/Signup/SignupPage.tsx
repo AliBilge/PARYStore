f34103abaeca22c0ea3f26f378940bfca890c250
poupakkg <60771085+poupakkg@users.noreply.github.com>
2020-04-26 23:59:41 -0600

    fixed errors in userprofile

--- src/components/Signup/SignupPage.tsx
+++ src/components/Signup/SignupPage.tsx
@@ -1,58 +1,96 @@
 import * as React from 'react';
 import { Fragment } from 'react';
 import { Form, Button } from 'semantic-ui-react';
-import { connect } from 'react-redux';
 import { RootState } from '../../store/users';
+import { connect } from 'react-redux';
+import { UserData } from '../../store/users/types';
+import { ADD_USER_TO_LIST, REMOVE_USER_FROM_LIST  } from '../../store/users/actions';
+
 
 export interface ISignupPageProps {
+      ADD_USER_TO_LIST: typeof  ADD_USER_TO_LIST,
+      userdata: UserData [],
+      id: number []
 }
 
-export  class SignupPage extends React.Component<ISignupPageProps> {
-   public render() {
-   
-    return (
-      <Fragment>
-          
-        <Form>
-              
-            <Form.Group unstackable widths={2}>
-                <Form.Input label='First name' placeholder='First name' />
-                <Form.Input label='Last name' placeholder='Last name' />
-            </Form.Group>
+export class SignupPage extends React.Component<ISignupPageProps>
+{
+      generateID = (): number => {
+            let newIdForUser = this.props.id.length +1;
+            newIdForUser += this.props.id.length;
+            return newIdForUser;
+      }
 
-            <Form.Group widths={2}>
-                <Form.Input label='Date of Birth' placeholder='Date of Birth' />
-                <Form.Input label='Gender' placeholder='Gender' />
-            </Form.Group>
+      newUser = ( event: any ) => {
+            event.preventDefault ();
+            const formField: HTMLInputElement | null = document.querySelector ('[name="userdata"]');
+            let formFieldValue: (UserData )=> string | number;
+            if (formField !=null ) formFieldValue = formField.value;
+            
+            this.props.ADD_USER_TO_LIST ({
 
-            <Form.Group widths={2}>
-                  <Form.Input label='Email' placeholder='Email' />    
-                  <Form.Input label='Phone' placeholder='Phone' />
-            </Form.Group>   
+                  id: this.generateID(),
+                  
+                  lastName: 'formFieldValue',
+                  firstName: 'formFieldValue',
+                  dateOfBirth: 'formFieldValue',
+                  gender: 'formFieldValue',
+                  email: 'formFieldValue',
+                  phone: formFieldValue,
+                  username: 'formFieldValue',
+                  password: 'formFieldValue'
+                          
+              });
+      }
 
-            <Form.Group widths={2}>
-                  <Form.Input label='Username' placeholder='Username' />
-                  <Form.Input label='Password' placeholder='Password' />
-            </Form.Group>   
+      public render() {
 
-                  <Form.Checkbox label='I agree to the Terms and Conditions' />
+            return (
+                  <Fragment>
 
-            <Button.Group>
-                  <Button type='submit' color ='green'>Sign Up</Button>
-                  <Button type='submit' color ='grey'>Reset</Button>
-            </Button.Group>
+                        <Form on submit = { this.newUser }>
+                             
+                              <Form.Group unstackable widths={2}>
+                                    <Form.Input label='First name' placeholder='First name' />
+                                    <Form.Input label='Last name' placeholder='Last name' />
+                              </Form.Group>
 
-        </Form>
-                   
-      </Fragment>
+                              {/* <Form.Group widths={2}>
+                                    <Form.Input label='Date of Birth' placeholder='Date of Birth' />
+                                    <Form.Input label='Gender' placeholder='Gender' /> */}
+                              {/* </Form.Group> */}
 
-    );
-  }
+                              <Form.Group widths={2}>
+                                    <Form.Input label='Email' placeholder='Email' />
+                                    <Form.Input label='Phone' placeholder='Phone' />
+                              </Form.Group>
+
+                              <Form.Group widths={2}>
+                                    <Form.Input label='Username' placeholder='Username' />
+                                    <Form.Input label='Password' placeholder='Password' />
+                              </Form.Group>
+
+                              <Form.Checkbox label='I agree to the Terms and Conditions' />
+                              <Button type='submit' class='float-left submit-button' color='green'> Sign Up </Button>
+                                
+                              }                                    
+                              
+                        </Form>
+
+                  </Fragment>
+
+            );
+      }
+
 }
-const mapStateToProps = (state: RootState) => {
+
+const mapStateToProps = ( state: RootState ) => {
       return {
-        UserData: state.users.usersdata
+        userdata: state.users.usersdata
       }
     }
     
-    export default connect(mapStateToProps)(SignupPage);
+      export default connect(
+      mapStateToProps,
+      { ADD_USER_TO_LIST, REMOVE_USER_FROM_LIST }
+    )( SignupPage );
--- src/store/users/reducers.ts
+++ src/store/users/reducers.ts
@@ -2,11 +2,11 @@ import { UserActionTypes, UserState, ADD_USER, REMOVE_USER } from './types';
 
 //input validation through github
 
-export const firstName = (value: string) => value && value.charAt(0).toUpperCase() + value.slice(1);
-export const lastName = (value: string) => value && value.charAt(0).toUpperCase() + value.slice(1);
-export const mobile = (value: string) => value.replace(/[^\d]/g, '').slice(0, 10);
-export const mobileNumber = (value: string) =>
-  value && !/^([6-9]\d{9})$/.test(value) ? 'Invalid mobile number, must be 10 digits' : undefined;
+// export const firstName = (value: string) => value && value.charAt(0).toUpperCase() + value.slice(1);
+// export const lastName = (value: string) => value && value.charAt(0).toUpperCase() + value.slice(1);
+// export const mobile = (value: string) => value.replace(/[^\d]/g, '').slice(0, 10);
+// export const mobileNumber = (value: string) =>
+//   value && !/^([6-9]\d{9})$/.test(value) ? 'Invalid mobile number, must be 10 digits' : undefined;
 
 // define initial state
 const initialState: UserState = {
